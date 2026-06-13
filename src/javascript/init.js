fcf89bd052ffcbce0ad32224fd9f4a8475639d49
Stephane Monier <smonier@jahia.com>
2026-06-13 14:40:15 +0200

    validated that requireModuleInstalledOnSite was true before rendering for site

--- src/javascript/init.js
+++ src/javascript/init.js
@@ -17,7 +17,7 @@ function registerTranslations() {
 
 function register() {
     registry.add('action', NS, {
-        targets: ['header-secondary-actions:999', 'contentActions:999'],
+        targets: ['headerPrimaryActions:999'],
         buttonIcon: window.jahia?.moonstone?.toIconComponent(
             '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="currentColor">' +
             '<path d="M20 2H4c-1.1 0-2 .9-2 2v18l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 14H5.17L4 17.17V4h16v12z"/>' +
